9af77e157b3b2729037d0888c8196f0278518dc7
Josh Heidenreich <josh.sickmate@gmail.com>
2012-04-30 17:15:28 +0930

    Added a footer line

--- src/foot.php
+++ src/foot.php
@@ -1,3 +1,9 @@
+<div id="foot">
+	Copyright &copy; <?php echo date('Y'); ?> Josh Heidenreich
+	&nbsp;
+	This website has a <a href="https://github.com/TheJosh/yafc-website">public git repo</a>.
+</div>
+
 </div>
 
 </body>
